80f7523215daba95a41bd4365e111983cf279047
dgh2 <2525915+dgh2@users.noreply.github.com>
2019-02-11 14:09:48 -0600

    Update shuntingYard.js

--- shuntingYard.js
+++ shuntingYard.js
@@ -107,6 +107,8 @@ class ShuntingYard {
         operators.push(Operator.createFunctionOperator("Σ", ShuntingYard.sum));
         operators.push(Operator.createBinaryOperator("ErroneousOperator", 2, undefined));
         operators.push(Operator.createUnaryPostfixOperator("ErroneousOperator", undefined));
+        operators.push(Operator.createConstant("🎱", ShuntingYard.magic8ball));
+        operators.push(Operator.createFunctionOperator("Magic8Ball", ShuntingYard.magic8ball));
 
         //sort by longest tokens first, so operators can be added in any order
         operators.sort(Operator.compareLength);
@@ -574,5 +576,30 @@ class ShuntingYard {
             return quantity.map(value => ShuntingYard.arrayDiceRoll(value, sides));
         }
         return ShuntingYard.diceRoll(Array(quantity).fill(1), sides);
+    }
+    
+    static magic8ball() {
+        return unaryDiceRoll(
+            ["It is certain.",
+             "It is decidedly so.",
+             "Without a doubt.",
+             "Yes - definitely.",
+             "You may rely on it.",
+             "As I see it, yes.",
+             "Most likely.",
+             "Outlook good.",
+             "Yes.",
+             "Signs point to yes.",
+             "Reply hazy, try again.",
+             "Ask again later.",
+             "Better not tell you now.",
+             "Cannot predict now.",
+             "Concentrate and ask again.",
+             "Don't count on it.",
+             "My reply is no.",
+             "My sources say no.",
+             "Outlook not so good.",
+             "Very doubtful."]
+        );
     }
 }
